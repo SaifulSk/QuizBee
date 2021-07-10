d216aaf0c0302d0f7eea8131a79f387645c66f3a
SaifulSk <ssaifrocks@gmail.com>
2021-07-10 11:58:59 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your name"/>
+    </div>
+  );
+}
+
+export default App;
